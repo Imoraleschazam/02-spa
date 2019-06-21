e471f2e6da36e282be24b1806f7c875a0aef8cdc
Ivan Morales <imorales@chazam.com.mx>
2019-06-21 18:19:34 -0500

    Configurando Rutas
    Semi Terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+
+
+
+
+//componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
